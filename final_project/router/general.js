646820d5e1827d018f19023973929b53576eed95
EigeneDateien <frederik.seib@gmx.de>
2024-01-12 11:20:07 -0500

    Using Promise for GET

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,57 +22,100 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,3));
+    new Promise((resolve, reject) => {
+        if (books) {
+            resolve(books);
+        } else {
+            reject("No books found");
+        }
+    })
+    .then((booksData) => {
+        res.send(JSON.stringify(booksData, null, 3));
+    })
+    .catch((error) => {
+        res.status(404).send(error);
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) {
+            resolve(book);
+        } else {
+            reject("Book not found for ISBN: " + isbn);
+        }
+    })
+    .then((bookData) => {
+        res.send(bookData);
+    })
+    .catch((errorMessage) => {
+        res.status(404).send(errorMessage);
+    });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     let requestedAuthor = req.params.author;
-    let booksByAuthor = {"booksbyauthor": []};
 
-    for (let key in books) {
-        if (books[key].author === requestedAuthor) {
-            booksByAuthor.booksbyauthor.push({
-                "isbn": key,
-                "title": books[key].title,
-                "reviews": books[key].reviews
-            });
+    new Promise((resolve, reject) => {
+        let booksByAuthor = {"booksbyauthor": []};
+
+        for (let key in books) {
+            if (books[key].author === requestedAuthor) {
+                booksByAuthor.booksbyauthor.push({
+                    "isbn": key,
+                    "title": books[key].title,
+                    "reviews": books[key].reviews
+                });
+            }
         }
-    }
 
-    if (booksByAuthor.booksbyauthor.length > 0) {
-        res.json(booksByAuthor);
-    } else {
-        res.status(404).send('No books found for the author: ' + requestedAuthor);
-    }
+        if (booksByAuthor.booksbyauthor.length > 0) {
+            resolve(booksByAuthor);
+        } else {
+            reject('No books found for the author: ' + requestedAuthor);
+        }
+    })
+    .then((booksData) => {
+        res.json(booksData);
+    })
+    .catch((errorMessage) => {
+        res.status(404).send(errorMessage);
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     let requestedTitle = req.params.title;
-    let booksByTitle = {"booksbytitle": []};
+    
+    new Promise((resolve, reject) => {
+        let booksByTitle = {"booksbytitle": []};
 
-    for (let key in books) {
-        if (books[key].title === requestedTitle) {
-            booksByTitle.booksbytitle.push({
-                "isbn": key,
-                "author": books[key].author,
-                "reviews": books[key].reviews
-            });
+        for (let key in books) {
+            if (books[key].title === requestedTitle) {
+                booksByTitle.booksbytitle.push({
+                    "isbn": key,
+                    "author": books[key].author,
+                    "reviews": books[key].reviews
+                });
+            }
         }
-    }
 
-    if (booksByTitle.booksbytitle.length > 0) {
-        res.json(booksByTitle);
-    } else {
-        res.status(404).send('No books found for the title: ' + requestedTitle);
-    }
+        if (booksByTitle.booksbytitle.length > 0) {
+            resolve(booksByTitle);
+        } else {
+            reject('No books found for the title: ' + requestedTitle);
+        }
+    })
+    .then((booksData) => {
+        res.json(booksData);
+    })
+    .catch((errorMessage) => {
+        res.status(404).send(errorMessage);
+    });
 });
 
 //  Get book review
