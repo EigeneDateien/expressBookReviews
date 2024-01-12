a59310de4858d72ffb45dfc781b451ab37d40e4c
Frederik Seib <frederik.seib@gmx.de>
2024-01-12 06:59:58 -0500

    Register new user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,20 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+    return users.some(user => user.username === username);
+}
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required." });
+    }
+    if (doesExist(username)) {
+        return res.status(409).json({ message: "Customer already exists!" });
+    }
+    users.push({ "username": username, "password": password });
+    return res.status(200).json({ message: "Customer successfully registered. Now you can login" });
 });
 
 // Get the book list available in the shop
