95f484bfa2abb6cd63d98c27f7a48464f69279dd
Frederik Seib <frederik.seib@gmx.de>
2024-01-12 06:33:50 -0500

    Get books by author and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,14 +23,46 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let requestedAuthor = req.params.author;
+    let booksByAuthor = {"booksbyauthor": []};
+
+    for (let key in books) {
+        if (books[key].author === requestedAuthor) {
+            booksByAuthor.booksbyauthor.push({
+                "isbn": key,
+                "title": books[key].title,
+                "reviews": books[key].reviews
+            });
+        }
+    }
+
+    if (booksByAuthor.booksbyauthor.length > 0) {
+        res.json(booksByAuthor);
+    } else {
+        res.status(404).send('No books found for the author: ' + requestedAuthor);
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let requestedTitle = req.params.title;
+    let booksByTitle = {"booksbytitle": []};
+
+    for (let key in books) {
+        if (books[key].title === requestedTitle) {
+            booksByTitle.booksbytitle.push({
+                "isbn": key,
+                "author": books[key].author,
+                "reviews": books[key].reviews
+            });
+        }
+    }
+
+    if (booksByTitle.booksbytitle.length > 0) {
+        res.json(booksByTitle);
+    } else {
+        res.status(404).send('No books found for the title: ' + requestedTitle);
+    }
 });
 
 //  Get book review
